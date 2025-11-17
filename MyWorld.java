f76f06fce9d2f9a0c7bc3ffb9090f6a5d47428be
Adeline Lai <adelinelai08@gmail.com>
2025-11-17 13:52:20 -0500

    Edited Elephant Class
    Made elephant move

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,17 @@
-import greenfoot.*;
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The World our hero lives in.
+ * 
+ * @author Adeline Lai
+ * @version November 2025
+ */
 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
     }
 }
