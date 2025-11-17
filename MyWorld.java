508f69218e1adbf8164128309d2db7d6a615d898
Adeline Lai <adelinelai08@gmail.com>
2025-11-17 14:04:04 -0500

    Added createApple method

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,17 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
-        Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = newApple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
