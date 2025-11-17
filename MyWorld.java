6bde7a71051fed95061c5067ae71edadbbf8e1b4
Adeline Lai <adelinelai08@gmail.com>
2025-11-17 14:02:10 -0500

    Added and edited Apple Class
    - Got it to move down and set its location

--- MyWorld.java
+++ MyWorld.java
@@ -8,10 +8,14 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {
         super(600, 400, 1);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
