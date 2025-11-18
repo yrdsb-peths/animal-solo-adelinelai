fc4eba47af1be0b860e12895859c7714321c24be
Adeline Lai <adelinelai08@gmail.com>
2025-11-18 13:54:02 -0500

    Imported Label Class

--- MyWorld.java
+++ MyWorld.java
@@ -15,6 +15,10 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
         
+        //Create a label
+        Label scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
         createApple();
     }
     
