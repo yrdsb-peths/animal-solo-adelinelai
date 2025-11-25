c222403eaee32bc58bd20f70b0d2a64108312426
Adeline Lai <adelinelai08@gmail.com>
2025-11-25 15:45:21 -0500

    Added varying game difficulties

--- MyWorld.java
+++ MyWorld.java
@@ -11,6 +11,8 @@ public class MyWorld extends World {
     
     public int score = 0;  
     Label scoreLabel;
+    int level = 1;
+    
     /**
      * Constructor for objects of class MyWorld.
      */
@@ -43,6 +45,11 @@ public class MyWorld extends World {
     {
         score++;
         scoreLabel.setValue(score);
+        
+        if(score % 5 == 0)
+        {
+            level += 1;
+        }
     }
     
     /**
@@ -51,6 +58,7 @@ public class MyWorld extends World {
     public void createApple()
     {
         Apple apple = new Apple();
+        apple.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
         addObject(apple, x, y);
